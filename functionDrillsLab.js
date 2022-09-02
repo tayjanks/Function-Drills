facfdc7ef9f062e6ad2cca3ce579a05f0d485ce1
tayjanks <taylorjenkins801@gmail.com>
2022-09-01 21:31:38 -0600

    problem 18 done!

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -317,6 +317,14 @@ let tryUpper = upperCase(sampleString);
   return 'must provide a valid email address'
 */
 
+function emailCheck (email){
+  email = String(email).trim();
+  if (email.includes("@"))
+{return "email verified"}
+else {return "must provide a valid email address"}
+}
+
+console.log (emailCheck ("taylor@gmail.com"))
 ////////////////// PROBLEM 19 ////////////////////
 /*
   Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were able to purchase.
